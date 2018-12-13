491c6eeaa2c96a2f0e2e6c44ccc2d5b4c91e9afe
BreadMan96 <vlad962706@mail.ru>
2018-12-13 19:18:32 +0300

    Add Actors migration

---
+++ db/migrate/20181213160720_create_actors.rb
@@ -0,0 +1,14 @@
+class CreateActors < ActiveRecord::Migration[5.2]
+  def change
+
+  	create_table :actors do |t|
+		t.text :name
+		t.text :phone
+		t.text :city
+		t.tetxt :role
+		
+		t.timestamps 
+	end
+  end
+  
+end
