0eb96e302939f453fe11049f91b710bf55a677ec
BreadMan96 <vlad962706@mail.ru>
2018-12-13 18:53:35 +0300

    Install Rakefile/Gems/nav

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:nagrani.db"
+
+class Client < ActiveRecord::Base
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/rec' do
+	erb "Rec"			
+end
+
+get '/story' do
+	erb "Story"			
+end
+
+get '/noname' do
+	erb "No Name"			
+end
