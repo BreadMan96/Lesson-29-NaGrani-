746c026bc952ef112f7414d2726b891cf83a902a
BreadMan96 <vlad962706@mail.ru>
2018-12-15 13:58:43 +0300

    Edit write BD (/rec - /script)

--- app.rb
+++ app.rb
@@ -14,21 +14,19 @@ end
 class Scripts < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@curators = Curators.all 
 	@actors = Actors.all
+	@scripts = Scripts.all
+end
+
+get '/' do
 	erb :index			
 end
 
 post '/rec' do
-	@actor_name = params[:actor_name]
-	@actor_age = params[:actor_age]
-	@actor_phone = params[:actor_phone]
-	@actor_city = params[:actor_city]
-	@actor_link = params[:actor_link]
-	@actor_story = params[:actor_story]
-
-	Actors.create :name => @actor_name, :age => @actor_age, :phone => @actor_phone, :city => @actor_city, :link => @actor_link, :role => @actor_story
+	a = Actors.new params[:actor]
+	a.save
 	
 	@title = "Ваша заявка успешно отправлена!"
 	@message = "Мы свяжемся с Вами в ближайшее время."
@@ -62,16 +60,13 @@ get '/script' do
 end
 
 get '/all' do
-	@actors = Actors.all
 	erb :all			
 end
 
 get '/success' do
-	@actors = Actors.all
 	erb :success			
 end
 
 get '/denied' do
-	@actors = Actors.all
 	erb :denied		
 end
